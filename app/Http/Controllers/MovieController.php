b5fd43b14e8c1b5f0a54aeebbaf7c27abe2dce74
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-04-21 14:33:25 +0200

    funzione controller e route

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index() {
+
+        // recupero dati
+
+        // return della vista
+
+        return view('home');
+    }
 }
